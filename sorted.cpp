36f7c66ee3082e5e1b6cfedef2826f4c5a63db7b
Sonnengott <80594111+Silas217209@users.noreply.github.com>
2024-09-21 15:43:24 +0200

    sorted von Koch

--- sorted.cpp
+++ sorted.cpp
@@ -1,35 +1,170 @@
-#include <string>
+#include <iostream>
+using namespace std;
 
 struct Person {
-    std::string NN;
-    std::string VN;
-    Person* next;
-    Person* back;
-} *head, *tail, *runner;
+    string NN;
+    string VN;
+    Person *next;
+    Person *back;
+} *head, *tail, *runner, *now;
+
+void initSorted();
+void insertElement(string Nachname, string Vorname);
+bool isEmpty();
+
+bool deleteElement(string Nachname);
+string searchElement(string Nachname);
+string backward();
+string forward();
+string first();
+string last();
+string current();
+string ausgabe();
+
+
+int main(int argc, const char *argv[]) {
+    initSorted();
+    insertElement("Funke", "Anton");
+    insertElement("Seifert", "Nils");
+    insertElement("Ramm", "Johann");
+    insertElement("Runge", "Emma");
+    insertElement("Dahl", "Torben");
+    insertElement("Hinz", "Max");
+    insertElement("Klitza", "Rouven");
+    insertElement("Pachali", "Silas");
+
+    insertElement("Formaniak", "Brooke");
+    insertElement("Schubert", "Lars");
+    insertElement("Theodor", "Tritthart");
+    insertElement("Uszpelkat", "Felix");
+
+    cout << searchElement("Formaniak");
+    cout << forward() << endl;
+    cout << forward() << endl;
+    deleteElement("Formaniak");
+    cout << backward() << endl;
+    cout << backward() << endl;
+    cout << last() << endl;
+    cout << first() << endl;
+    cout << searchElement("Formaniak");
+
+
+    return 0;
+}
+bool deleteElement(string Nachname) {
+    runner = head->next;
+    while (runner->NN != Nachname && runner != tail) {
+        runner = runner->next;
+    }
+    if (runner->NN == Nachname) {
+        runner->back->next = runner->next;
+        runner->next->back = runner->back;
+        delete runner;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+string searchElement(string Nachname) {
+    runner = head->next;
+    string merke;
+    while (runner->NN != Nachname && runner != tail) {
+        runner = runner->next;
+    }
+    if (runner->NN == Nachname) {
+        merke = runner->VN;
+        return merke;
+    } else {
+        return "nicht drin";
+    }
+};
+string backward() {
+    if (now->back != head) {
+        now = now->back;
+        return ausgabe();
+    } else {
+        return "ist erstes Element";
+    }
+};
+string forward() {
+    if (now->next != tail) {
+        now = now->next;
+        return ausgabe();
+    } else {
+        return "ist letztes Element";
+    }
+};
+string first() {
+    now = head->next;
+    if (now != tail) {
+        return ausgabe() + " ist am Anfang";
+        ;
+    } else {
+        return "Liste leer";
+    }
+};
+string last() {
+    now = tail->back;
+    if (now != head) {
+        return ausgabe() + " ist am Ende";
+        ;
+    } else {
+        return "Liste leer";
+    }
+};
+string current() {
+    if (now != head && now != tail) {
+        return ausgabe();
+    } else {
+        return "Liste leer";
+    }
+};
+string ausgabe() { return now->NN + " ; " + now->VN; };
+
 
 void initSorted() {
+    // Speicher
     head = new Person;
-   tail = new Person;
-
-   head->NN = "";
-   head->VN = "";
-
-   tail->NN = "";
-   tail->VN = "";
-
-   head->next = tail;
-   head->back = NULL;
-
-   tail->next = NULL;
-   tail->back = head;
-
-    runner = head;
+    tail = new Person;
+    // Werte
+    head->NN = "AAAnfang";
+    head->VN = "AAAnfang";
+    tail->NN = "zzzuEnde";
+    tail->VN = "zzzuEnde";
+    // Verknüpfungen
+    head->next = tail;
+    tail->back = head;
+    now = head;
 }
-void insertElement() {}
+void insertElement(string Nachname, string Vorname) {
+    // Speicher
+    Person *dummy = new Person;
+    // Werte
+    dummy->NN = Nachname;
+    dummy->VN = Vorname;
+    if (isEmpty()) {
+        dummy->next = tail;
+        dummy->back = head;
+        head->next = dummy;
+        tail->back = dummy;
+    } else {
+        runner = head->next;
+        while (Nachname > runner->NN && runner != tail) {
+            runner = runner->next;
+        }
+        dummy->next = runner;
+        dummy->back = runner->back;
+        dummy->back->next = dummy;
+        runner->back = dummy;
+    }
+    now = dummy;
+}
+
 bool isEmpty() {
-   return head->next == tail;
-}
-
-int main() {
-    return 1:
+    if (head->next == tail) {
+        return true;
+    } else {
+        return false;
+    }
 }
